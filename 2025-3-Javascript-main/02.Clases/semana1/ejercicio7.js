6cd61204ae0173202d419fefc67ca88b5fae3608
Juan Antonio Mejia Peralta <jamp992112@gmail.com>
2025-08-18 19:43:43 -0600

    Semana2

---
+++ 2025-3-Javascript-main/02.Clases/semana1/ejercicio7.js
@@ -0,0 +1,10 @@
+// Clase 3: Lógica con Condicionales
+const { ask } = require('../helpers/input');
+
+async function main() {
+  const number = Number(await ask("Ingresa un número:"));
+
+  if(number % 3 !==0 && number %5 )
+}
+
+main();
